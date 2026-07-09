d9ad3b20f0e0ef6e13105e363176a41e6c5e73eb
Tran Khiet Loi <loi.tran@lfglobaltech.com>
2026-07-09 15:36:15 +0700

    feat(Permission, AssignPermissionToRole): Add service and clean code

---
+++ MySolution.Infrastructure/Migrations/20260709032222_SecondCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MySolution.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Permissions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true,
+                oldDefaultValueSql: "now()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Permissions",
+                type: "timestamp with time zone",
+                nullable: true,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "now()");
+        }
+    }
+}
