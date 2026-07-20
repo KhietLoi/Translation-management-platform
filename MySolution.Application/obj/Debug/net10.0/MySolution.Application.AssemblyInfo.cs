6a7e89a257ee3b85a72cea9654cd4ad2d3ce5957
Tran Khiet Loi <loi.tran@lfglobaltech.com>
2026-07-20 15:33:03 +0700

    feat(MassTransit): Add MassTransit integration and configure email queues for verification and password events

--- MySolution.Application/obj/Debug/net10.0/MySolution.Application.AssemblyInfo.cs
+++ MySolution.Application/obj/Debug/net10.0/MySolution.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MySolution.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dbf38bd403741fd250b65c05ab507db9a7f11b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb6099ab2ad17963d5e63c480a9a3da84a6f434")]
 [assembly: System.Reflection.AssemblyProductAttribute("MySolution.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MySolution.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ MySolution.Infrastructure/MassTransit/MassTransitRegistration.cs
@@ -0,0 +1,66 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MySolution.Application.Common.Interfaces.MassTransit;
+using MySolution.Infrastructure.MassTransit.Consumers;
+using MySolution.Infrastructure.Options;
+using Shared.MassTransit;
+using Shared.MassTransit.IntegrationEvents;
+
+namespace MySolution.Infrastructure.MassTransit;
+
+public static class MassTransitRegistration
+{
+    public static IServiceCollection AddMassTransitServices
+    (
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        var rabbitMqOptions = configuration
+            .GetSection(RabbitMqOptions.SectionName)
+            .Get<RabbitMqOptions>() ?? throw new InvalidOperationException("RabbitMQ configuration missing");
+        
+        services.AddMassTransit(x =>
+        {
+            x.AddConsumer<SendVerifyEmailConsumer>();
+            x.AddConsumer<SendSetupPasswordEmailConsumer>();
+            x.AddConsumer<SendForgotPasswordEmailConsumer>();
+
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host(
+                    rabbitMqOptions.Host,
+                    "/",
+                    h =>
+                    {
+                        h.Username(rabbitMqOptions.Username);
+                        h.Password(rabbitMqOptions.Password);
+                    });
+
+                ConfigureEmailQueues(context, cfg);
+            });
+        });
+        services.AddScoped<IMessageSender, SendEndPointCustomProvider>();
+        
+        return services;
+    }
+
+    private static void ConfigureEmailQueues(IBusRegistrationContext context,
+        IRabbitMqBusFactoryConfigurator cfg)
+    {
+        cfg.ReceiveEndpoint(QueueNameHelper.Get<SendVerifyEmailEvent>(), e =>
+        {
+            e.ConfigureConsumer<SendVerifyEmailConsumer>(context);
+        });
+        cfg.ReceiveEndpoint(QueueNameHelper.Get<SendSetUpPasswordEmailEvent>(), e =>
+        {
+            e.ConfigureConsumer<SendSetupPasswordEmailConsumer>(context);
+        });
+        cfg.ReceiveEndpoint(QueueNameHelper.Get<SendForgotPasswordEmailEvent>(), e =>
+        {
+            e.ConfigureConsumer<SendForgotPasswordEmailConsumer>(context);
+        });
+    }
+    
+}
