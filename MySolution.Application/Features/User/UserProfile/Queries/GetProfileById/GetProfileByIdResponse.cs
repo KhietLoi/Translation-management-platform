5b3712d411a30ec25c98958d79e9e553a4bc4bd0
Tran Khiet Loi <loi.tran@lfglobaltech.com>
2026-07-23 17:56:19 +0700

    feat(UserProfile): Implement GetProfileById query and UserProfileController for user profile management

---
+++ MySolution.Application/Features/User/UserProfile/Queries/GetProfileById/GetProfileByIdResponse.cs
@@ -0,0 +1,18 @@
+﻿using MySolution.Application.Common.Models;
+
+namespace MySolution.Application.Features.User.UserProfile.Queries.GetProfileById;
+
+public class GetProfileByIdResponse : BaseResponse <GetProfileByIdData>
+{
+
+}
+
+public class GetProfileByIdData
+{
+    public Guid UserId { get; set; }
+    public string? FullName { get; set; } 
+    public DateOnly? BirthDate { get; set; }
+    public string? PhoneNumber { get; set; } 
+    public string? AvatarBlobName { get; set; }
+    public string? Address { get; set; }
+}
