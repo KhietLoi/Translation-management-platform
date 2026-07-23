5d08ae39dcd6ee4250b5eed79ab59565ee87a8ac
Tran Khiet Loi <loi.tran@lfglobaltech.com>
2026-07-23 16:44:42 +0700

    feat(UserProfile): Implement UpdateProfile command with request and response models

---
+++ MySolution.Application/Features/User/UserProfile/Commands/UpdateProfile/UpdateProfileCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace MySolution.Application.Features.User.UserProfile.Commands.UpdateProfile;
+
+public class UpdateProfileCommand : IRequest<UpdateProfileResponse>
+{
+    public UpdateProfileRequest Payload { get; set; }
+
+    public UpdateProfileCommand(UpdateProfileRequest payload)
+    {
+        Payload = payload;
+    }
+}
---
+++ MySolution.Application/Features/User/UserProfile/Commands/UpdateProfile/UpdateProfileRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace MySolution.Application.Features.User.UserProfile.Commands.UpdateProfile;
+
+public class UpdateProfileRequest
+{
+
+}
