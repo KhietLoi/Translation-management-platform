f5dd1531c001ae7c206c3ce84872e57edc7c3d41
Tran Khiet Loi <loi.tran@lfglobaltech.com>
2026-08-03 15:43:16 +0700

    feat(project-full): Implement CreateProjectFull command, handler, request, response, and validator for full project creation

---
+++ MySolution.Application/Features/Project/Commands/CreateProjectFull/CreateProjectFullResponse.cs
@@ -0,0 +1,14 @@
+﻿using MySolution.Application.Common.Models;
+
+namespace MySolution.Application.Features.Project.Commands.CreateProjectFull;
+
+public class CreateProjectFullResponse : BaseResponse <CreateProjectFullData>
+{
+
+}
+
+public class CreateProjectFullData
+{
+    public Guid ProjectId { get; set; }
+    public string ProjectName { get; set; } = string.Empty;
+}
