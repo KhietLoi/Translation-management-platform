5f239da49700ab81902bd31252bdb86dafca6631
Tran Khiet Loi <loi.tran@lfglobaltech.com>
2026-08-10 17:26:06 +0700

    feat(publish): Add IPublishService interface for publishing functionality

---
+++ MySolution.Application/Common/Interfaces/File/IPublishService.cs
@@ -0,0 +1,8 @@
+﻿using MySolution.Application.Common.Models;
+
+namespace MySolution.Application.Common.Interfaces.File;
+
+public interface IPublishService
+{
+    Task <PublishStatistics> PublishAsync(Guid projectId, Guid publishedBy,  string? notes,  CancellationToken cancellationToken);
+}
