eedb91151e3ec81a33835fbaa23ced9b0d146f72
Tran Khiet Loi <loi.tran@lfglobaltech.com>
2026-08-12 15:25:24 +0700

    feat(translation-pipeline): Add GetReleaseDiff query and handler for comparing translation releases

---
+++ MySolution.Application/Features/TranslationPipeline/Queries/GetReleaseDiff/GetReleaseDiffValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace MySolution.Application.Features.TranslationPipeline.Queries.GetReleaseDiff;
+
+public class GetReleaseDiffValidator : AbstractValidator<GetReleaseDiffQuery>
+{
+    public GetReleaseDiffValidator()
+    {
+        RuleFor(x => x.SourceReleaseId)
+            .NotEmpty()
+            .WithMessage("The source release id cannot be empty");
+    }
+}
