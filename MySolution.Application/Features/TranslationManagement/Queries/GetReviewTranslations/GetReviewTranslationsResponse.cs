b94e1a7c0927f432d1916f877a6aa4cbfe6a8c8c
LoiCT2709 <khietloi2004@gmail.com>
2026-08-16 01:15:20 +0700

    feat(translation-management): Implement BatchReviewTranslation command and handler, add request and response models for batch translation review

---
+++ MySolution.Application/Features/TranslationManagement/Queries/GetReviewTranslations/GetReviewTranslationsResponse.cs
@@ -0,0 +1,22 @@
+﻿using MySolution.Application.Common.Models;
+
+namespace MySolution.Application.Features.TranslationManagement.Queries.GetReviewTranslations;
+
+public class GetReviewTranslationsResponse : BaseResponse <GetReviewTranslationsData>
+{
+
+}
+
+public class GetReviewTranslationsData
+{
+    public List<TranslationItem> Items { get; set; } = [];
+}
+
+public class TranslationItem
+{
+    public Guid TranslationValueId { get; set; }
+    public Guid TranslationKeyId { get; set; }
+    public string Key { get; set; } = string.Empty;
+    public string Value { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+}
